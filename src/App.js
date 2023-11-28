259e4f481d24376a65b54735242b7e887ad1c82e
Alexander Khvoshchev <shsha.khv@gmail.com>
2023-11-28 03:34:45 +0300

    container with some css and input boxes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h2 className="title">Decimal - Binary Converter</h2>
+
+        <div>
+          <div className="input-wrapper">
+            <label for="dec-inp">Decimal:</label>
+            <input type="number" id="dec-inp" />
+          </div>
+          <div className="input-wrapper">
+            <label for="bin-inp">Binary:</label>
+            <input type="number" id="bin-inp" />
+          </div>
+        </div>
+        <p id="error-msg">Sample Error Message</p>
+      </div>
     </div>
   );
 }
